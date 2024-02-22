5a32401f750f65fd0e1d93a498901f7922f11010
dalix7 <letaiefdali@gmail.com>
2024-02-22 21:49:15 +0100

    pannier

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
+/* declaration variable */
+
 var plusBtns = Array.from(document.querySelectorAll('.fa-plus-circle'))
 var minusBtns = Array.from(document.querySelectorAll('.fa-minus-circle'))
 var reactBtns = Array.from(document.querySelectorAll('.fa-heart'))
 var deleteBtns = Array.from(document.querySelectorAll('.fa-trash-alt'))
 var cards = Array.from(document.querySelectorAll('.mb-3'))
+
+/* bouton '+' action */
 for (let plus of plusBtns) {
   plus.addEventListener('click', function () {
     plus.nextElementSibling.innerHTML++
     total()
   })
 }
+/* bouton '-' action */
 for (let minus of minusBtns) {
   minus.addEventListener('click', function () {
     minus.previousElementSibling.innerHTML > 0
       ? minus.previousElementSibling.innerHTML--
       : null
+    /* appel du fonction somme */
     total()
   })
 }
-
+/* bouton 'suprimer' action */
 for (let i in deleteBtns) {
   deleteBtns[i].addEventListener('click', function () {
     cards[i].remove()
+    /* appel du fonction somme */
     total()
   })
 }
+/* somme fonction */
 function total () {
   let qte = Array.from(document.querySelectorAll('.qute'))
   let price = Array.from(document.querySelectorAll('.unitt-price'))
@@ -33,14 +41,8 @@ function total () {
   }
   document.getElementById('total-price').innerHTML = s
 }
-/* 
-for (let heart of reactBtns) {
-  heart.addEventListener('click', function () {
-    heart.style.Color == 'black'
-      ? (heart.style.Color = 'red')
-      : (heart.style.Color = 'black')
-  })
-} */
+/* bouton heart reaction red, black*/
+
 for (let heart of reactBtns)
   heart.addEventListener('click', function () {
     if (heart.style.color == 'black') {
@@ -49,10 +51,3 @@ for (let heart of reactBtns)
       heart.style.color = 'black'
     }
   })
-/* for (let j = 0; j < heart.length; j++) {
-  heart[j].addEventListener('click', function () {
-    heart[j].style.color == 'black'
-      ? (heart[j].style.color = 'red')
-      : (heart[j].style.color = 'black')
-  })
-} */
